bc1fafdbe05542d3e9b10ff0f51f6486034b1f57
Ion Ureche <ureche.ion@gmail.com>
2019-05-20 21:39:15 +0300

    Added freq bands

--- Spectralizer/Assets/Scripts/Spectralizer.cs
+++ Spectralizer/Assets/Scripts/Spectralizer.cs
@@ -7,21 +7,40 @@ public class Spectralizer : MonoBehaviour
 {
 	AudioSource _audioSource;
 	public static float[] _samples = new float[512];
+	public static float[] _freqBands = new float[8];
 	// Start is called before the first frame update
 	void Start()
     {
 		_audioSource = GetComponent<AudioSource>();
-
 	}
 
     // Update is called once per frame
     void Update()
     {
 		GetSpectrumAudioSource();
+		MakeFrequencyBands();
 	}
 
 	void GetSpectrumAudioSource()
 	{
 		_audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+	}
+
+	void MakeFrequencyBands()
+	{
+		int count = 0;
+		for(int i = 0; i < 8; ++i)
+		{
+			float average = 0;
+			int sampleCount = (int)Mathf.Pow(2, i+1);
+			if (i == 7) sampleCount += 2;
+			for(int j = 0; j < sampleCount; ++j)
+			{
+				average += _samples[count] * (count + 1);
+				count++;
+			}
+			average /= count;
+			_freqBands[i] = average *  10;
+		}
 	}
 }
