466c5390568b7fefe647395ec1527c052ce7764f
Ion Ureche <ureche.ion@gmail.com>
2019-05-20 21:19:28 +0300

    Added basic spectrum visualizer

---
+++ Spectralizer/Assets/Scripts/InstantiateCubes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateCubes : MonoBehaviour
+{
+	public GameObject _sampleCubePrefab;
+	GameObject[] _sampleCube = new GameObject[512];
+	public float _maxScale = 1000;
+	// Start is called before the first frame update
+	void Start()
+    {
+        for(int i = 0; i < 512; ++i)
+		{
+			GameObject _instance = (GameObject)Instantiate(_sampleCubePrefab);
+			_instance.transform.position = this.transform.position;
+			_instance.transform.parent = this.transform;
+			_instance.name = "sampleCube" + i;
+			this.transform.eulerAngles = new Vector3(0, -0.703125f * i, 0);
+			_instance.transform.position = Vector3.forward * 100;
+			_sampleCube[i] = _instance;
+		}
+    }
+
+	// Update is called once per frame
+	void Update()
+	{
+		for (int i = 0; i < 512; ++i)
+		if(_sampleCube[i] != null)
+		{
+			_sampleCube[i].transform.localScale = new Vector3(2, Spectralizer._samples[i] * _maxScale, 2);
+		}
+	}
+}
--- Spectralizer/Assets/Scripts/Spectralizer.cs
+++ Spectralizer/Assets/Scripts/Spectralizer.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof (AudioSource))]
 public class Spectralizer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+	AudioSource _audioSource;
+	public static float[] _samples = new float[512];
+	// Start is called before the first frame update
+	void Start()
     {
-        
-    }
+		_audioSource = GetComponent<AudioSource>();
+
+	}
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+		GetSpectrumAudioSource();
+	}
+
+	void GetSpectrumAudioSource()
+	{
+		_audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+	}
 }
